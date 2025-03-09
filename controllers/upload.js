6264be24d1345e35e02c9992974e8db261157609
zelkim <dev86.sean@gmail.com>
2025-03-09 13:49:05 +0800

    debug: printed req.user

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import Image from '../model.js';
-import upload from '../services/storage.js'
-import accessValidation from '../middleware/accessValidation.js';
-import path from 'path';
-import sharp from 'sharp';
-import fs from 'fs';
+import express from "express";
+import Image from "../model.js";
+import upload from "../services/storage.js";
+import accessValidation from "../middleware/accessValidation.js";
+import path from "path";
+import sharp from "sharp";
+import fs from "fs";
 
 const router = express.Router();
 
@@ -17,41 +17,58 @@ router.post("/upload", accessValidation, async (req, res) => {
       const { file } = req;
       const uploadData = req.body;
       if (!file) {
-        return res.status(400).json({ success: false, message: 'file not supplied' });
+        return res
+          .status(400)
+          .json({ success: false, message: "file not supplied" });
       }
-      const mainFilePath = path.join(process.env.IMAGE_STORE_PATH, 'M_' + file.filename);
-      const thumbFilePath = path.join(process.env.IMAGE_STORE_PATH, 'T_' + file.filename);
+      const mainFilePath = path.join(
+        process.env.IMAGE_STORE_PATH,
+        "M_" + file.filename,
+      );
+      const thumbFilePath = path.join(
+        process.env.IMAGE_STORE_PATH,
+        "T_" + file.filename,
+      );
 
-      await sharp(file.path).resize().jpeg({ quality: 70 }).toFile(mainFilePath);
-      await sharp(file.path).resize().jpeg({ quality: 30 }).toFile(thumbFilePath);
+      await sharp(file.path)
+        .resize()
+        .jpeg({ quality: 70 })
+        .toFile(mainFilePath);
+      await sharp(file.path)
+        .resize()
+        .jpeg({ quality: 30 })
+        .toFile(thumbFilePath);
 
-      console.log(file)
+      console.log(file);
       fs.unlinkSync(file.path);
 
       const data = req.body;
 
+      console.log(req.user);
+
       await Image.create({
-        type: data.type || 'default',
+        type: data.type || "default",
         image: mainFilePath,
         thumbnail: thumbFilePath,
         metadata: {
           uploader: {
             id: req.user._id,
-            username: req.user.credentials.email
+            username: req.user.credentials.email,
           },
           // owners: uploadData.metadata.owners
-        }
+        },
       }).then((imageKey) => {
         res.status(201).json({
-          status: 'success',
+          status: "success",
           image: `${process.env.API_URL}/${imageKey._id}`,
-          thumbnail: `${process.env.API_URL}/${imageKey._id}?type=thumbnail`
+          thumbnail: `${process.env.API_URL}/${imageKey._id}?type=thumbnail`,
         });
       });
-
     } catch (error) {
-      console.error(error)
-      return res.status(500).json({ success: false, message: "Internal server error" });
+      console.error(error);
+      return res
+        .status(500)
+        .json({ success: false, message: "Internal server error" });
     }
   });
 });
